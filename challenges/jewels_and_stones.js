fac12aa3290baeee8b2a018d3cf72f857f203d9d
Bo <pangb86@gmail.com>
2018-03-02 13:54:55 -0800

    Add jewels and stones

---
+++ challenges/jewels_and_stones.js
@@ -0,0 +1,19 @@
+// You're given strings J representing the types of stones that are
+// jewels, and S representing the stones you have.  Each character in
+// S is a type of stone you have.  You want to know how many of the
+// stones you have are also jewels.
+//
+// The letters in J are guaranteed distinct, and all characters in J
+// and S are letters. Letters are case sensitive, so "a" is considered
+// a different type of stone from "A".
+
+var numJewelsInStones = function(J, S) {
+    var total = 0;
+    for (var i = 0; i < S.length; i++) {
+        if (J.includes(S[i])) {
+            total += 1;
+        }
+    }
+
+    return total;
+};
