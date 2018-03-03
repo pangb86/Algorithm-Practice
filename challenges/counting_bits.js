2f6c9ed638c647c8448c3530a5b0459d5979d918
Bo <pangb86@gmail.com>
2018-03-03 13:20:52 -0800

    Add counting bits

---
+++ challenges/counting_bits.js
@@ -0,0 +1,22 @@
+// Given a non negative integer number num. For every numbers i in the
+// range 0 ≤ i ≤ num calculate the number of 1's in their binary
+// representation and return them as an array.
+
+var countBits = function(num) {
+  let result = [];
+
+  for (var j = 0; j <= num; j++) {
+    const num_bin = j.toString(2);
+    let count = 0;
+
+    for (var i = 0; i < num_bin.length; i++) {
+      if (num_bin[i] == "1") {
+        count += 1;
+      }
+    }
+
+    result.push(count);
+  }
+
+  return result;
+};
