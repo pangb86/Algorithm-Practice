60d67eae32bf69922c2261a10a8eae10cf2d7bfa
Bo <pangb86@gmail.com>
2018-03-10 19:30:43 -0800

    add letter generator

---
+++ challenges/letter_generator.js
@@ -0,0 +1,18 @@
+// write a function that takes in a string argument and return a
+// generator where it will return the letters of the string argument
+// one letter at a time in order
+
+function letterGenerator(word) {
+  let index = 0;
+
+  return {
+    next: () => {
+      if (index < word.length) {
+        index++;
+        return word[index - 1];
+      } else {
+        return "";
+      }
+    }
+  };
+}
