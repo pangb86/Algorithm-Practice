7dda98b8c87a189c723990e5fb761021de8d050c
Bo <pangb86@gmail.com>
2018-03-21 16:35:13 -0700

    Add count prime set bits

---
+++ challenges/primes_of_set_bits_binary.js
@@ -0,0 +1,40 @@
+// Given two integers L and R, find the count of numbers in the
+// range [L, R] (inclusive) having a prime number of set bits in their
+// binary representation.
+//
+// (Recall that the number of set bits an integer has is the number
+// of 1s present when written in binary. For example, 21 written in
+// binary is 10101 which has 3 set bits. Also, 1 is not a prime.)
+
+var countPrimeSetBits = function(L, R) {
+  let count = 0;
+
+  for (let i = L; i <= R; i++) {
+    let binString = (i).toString(2);
+    let ones = 0;
+    for (let j = 0; j < binString.length; j++) {
+      if (binString[j] === "1") {
+        ones++;
+      }
+    }
+    if (isPrime(ones)) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+var isPrime = function(num) {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
