6c38136d65b05aab46626fbc5802e8a0ae3081f6
Bo <pangb86@gmail.com>
2018-03-23 16:14:50 -0700

    Add rotate string comments

--- challenges/rotate_string.js
+++ challenges/rotate_string.js
@@ -6,16 +6,19 @@
 // only if A can become B after some number of shifts on A.
 
 var rotateString = function(A, B) {
+  // return false if the given strings are not the same length
   if (A.length !== B.length) {
     return false;
   }
-
+  // iterate over string A
   for (var i = 0; i < A.length; i++) {
+    // return true if A equals B
     if (A === B) {
       return true;
     }
+    // move the first character of A to the end
     A = A.slice(1) + A[0];
   }
-
+  // return false if A cannot equal B after the rotations
   return false;
 };
