f7ba1b386996d5f7f7912a33f21ad97524e8eac4
Bo <pangb86@gmail.com>
2018-04-30 23:58:15 -0700

    Add staircase & counting zeroes

---
+++ challenges/counting_zeros.js
@@ -0,0 +1,10 @@
+function countZero(n) {
+  let count = 0;
+
+  while (n > 0) {
+   count += Math.floor(n / 10);
+   n = n / 10;
+  }
+  
+  return count;
+}
---
+++ challenges/staircase.js
@@ -0,0 +1,10 @@
+function main(n) {
+  for (let i = 1; i <= n; i++) {
+    let line = Array(n).fill(' ');
+    
+    for (let j = 0; j < i; j++) {
+      line[j] = '#';
+    }
+    console.log(line.reverse().join(''));
+    }
+}
