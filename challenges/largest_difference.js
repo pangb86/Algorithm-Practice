166c8bc0fa1ca1e71153686d44bdcc608c46ba92
Bo <pangb86@gmail.com>
2018-05-05 22:45:00 -0700

    Add largest difference

---
+++ challenges/largest_difference.js
@@ -0,0 +1,26 @@
+function largestDifference(array) {
+  // If there is only one element, there is no difference
+  if (array.length <= 1) return -1;
+
+  // currentMin will keep track of the current lowest
+  let currentMin = array[0];
+  let currentMaxDifference = 0;
+
+  // We will iterate through the array and keep track of the current max difference
+  // If we find a greater max difference, we will set the current max difference to that variable
+  // Keep track of the current min as we iterate through the array, since we know the greatest
+  // difference is yield from `largest value in future` - `smallest value before it`
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > currentMin && (array[i] - currentMin > currentMaxDifference)) {
+      currentMaxDifference = array[i] - currentMin;
+    } else if (array[i] <= currentMin) {
+      currentMin = array[i];
+    }
+  }
+
+  // If negative or 0, there is no largest difference
+  if (currentMaxDifference <= 0) return -1;
+
+  return currentMaxDifference;
+}
---
+++ challenges/product_except_self.js
@@ -0,0 +1,24 @@
+function productExceptSelf(numArray) {
+	let product = 1;
+	let size = numArray.length;
+	let output = [];
+
+  // From first array: [1, 2, 4, 16]
+  // The last number in this case is already in the right spot (allows for us)
+  // to just multiply by 1 in the next step.
+  // This step essentially gets the product to the left of the index at index + 1
+	for (let x = 0; x < size; x++) {
+		output.push(product);
+		product = product * numArray[x];
+	}
+
+  // From the back, we multiply the current output element (which represents the product
+  // on the left of the index, and multiplies it by the product on the right of the element)
+	let product = 1;
+	for (let i = size - 1; i > -1; i--) {
+		output[i] = output[i] * product;
+		product = product * numArray[i];
+	}
+
+  return output;
+}
