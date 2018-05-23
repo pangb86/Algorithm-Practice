2c263593fb17c76d9c52baf809bdecbc9d152ecd
Bo <pangb86@gmail.com>
2018-05-22 20:40:05 -0700

    Add alternate solution to palindrome permutation

--- challenges/CCI_palindrome_permutation.js
+++ challenges/CCI_palindrome_permutation.js
@@ -32,3 +32,32 @@ function permPalindrome(str) {
 }
 // O(n) time complexity
 // O(1) space complexity due to finite character set
+
+// check odd letter count as we count the letters
+function palindromPerm(str) {
+  let oddCount = 0;
+  let letterCount = {};
+  // iterate over the input string
+  for (let i = 0; i < str.length; i++) {
+    // count the frequency of each letter
+    let letter = str[i];
+    if (letterCount[letter] === undefined) {
+      letterCount[letter] = 0;
+    }
+    letterCount[letter]++;
+    // check the count of the current letter
+    if (letterCount[letter] % 2 === 1) {
+      // if it's odd, increment the odd counter
+      oddCount++;
+    } else {
+      // if it's even, decrement the odd counter
+      oddCount--;
+    }
+  }
+  // return true if odd count is 1 or less
+  return oddCount <= 1;
+}
+// O(n) time complexity
+// O(1) space complexity due to finite character set
+// not necessarily better than checking the count after counting
+// more operations per character
